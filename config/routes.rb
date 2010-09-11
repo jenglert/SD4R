92ce7a8ae198eee9ce7e7fd5eb1a3268ba0c5021
Jim Englert <jimenglert@jim-englerts-macbook.local>
2010-09-11 14:16:35 -0400

    Pushing a bit of a layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -40,6 +39,10 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+    map.resources :users
+    
+    map.root :controller => "homepage"
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
