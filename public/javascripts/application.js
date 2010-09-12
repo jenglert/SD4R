b402dfc5906dfd75bd84447ead82fe56f9b791ce
Jim Englert <jimenglert@jim-englerts-macbook.local>
2010-09-12 12:37:49 -0400

    fixing the way it looks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(document).ready(function() {
+	$("input:submit,").button();
+})
 
 function selectACity() {
 	var city = $('#city').val();
