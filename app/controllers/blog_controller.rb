e39a61e2fc0e72030dc26b9abcc10fa83dffb307
Jim Englert <jimenglert@jim-englerts-macbook.local>
2010-11-25 12:35:55 -0600

    Adding RSS feeds for the vroommates blog.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -8,4 +8,12 @@ class BlogController < ApplicationController
   def show
     @blog = BlogPost.find(params[:id])
   end
+  
+  def rss
+    @blogs = BlogPost.find(:all, :order => 'created_at desc', 
+        :conditions => ["live_date < ? or live_date is null", Time.now])
+        
+    render :layout => false
+    response.headers["Content-Type"] = "application/xml; charset=utf-8"
+  end
 end
