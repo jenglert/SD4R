597c240e96255234feaa932cc665579b62d32529
Jim Englert <englert.james@gmail.com>
2012-02-11 14:15:25 -0500

    Fixing migration

--- db/migrate/20101128011904_add_fields_to_events.rb
+++ db/migrate/20101128011904_add_fields_to_events.rb
@@ -1,7 +1,7 @@
 class AddFieldsToEvents < ActiveRecord::Migration
   def self.up
     add_column :events, :location, :text
-    add_column :events, :host_name, :sting
+    add_column :events, :host_name, :string
     add_column :events, :host_email, :string
     add_column :events, :host_phone, :string
     add_column :events, :notes, :text
